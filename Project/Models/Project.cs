8f05add078f40d5a09237fcc55bb7c331aaec4f4
Erika Debelis <erika.debelis@gmail.com>
2021-07-22 11:37:32 -0700

    add generic start up code + namespace / file frameworks

--- Project/Models/Project.cs
+++ Project/Models/Project.cs
@@ -1,12 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Triangle.Tracker
+namespace Name.Models
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+  class Program
+  {
+    
+  }
 }
